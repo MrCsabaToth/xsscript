d6f74c395a92caf5e91c881b8fc898bff22a0c51
MrCsabaToth <csaba.toth.us@gmail.com>
2016-04-05 20:18:10 -0700

    height adjust

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 //$(document).ready(function(){$("body").empty();$("body").html('<iframe style="width:100%;height:100%;position: absolute;" src="https://bulldogearthday.github.io/"></iframe>');});
-$(document).ready(function(){$(".main-content").empty().html('<iframe style="width:100%;height:100%;" src="https://bulldogearthday.github.io/"></iframe>');});
+$(document).ready(function(){$(".main-content").empty().html('<iframe style="width:100%;height:600px" src="https://bulldogearthday.github.io/"></iframe>');});
